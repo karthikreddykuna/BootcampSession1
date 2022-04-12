d4bc67174a3f4e3703cbb741821fe1f16613f190
root <root@adf48253c147>
2022-04-12 14:51:37 +0000

    Model Deployment

--- Model/best_pipeline.py
+++ Model/best_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('BootcampSession1/Model/prepared_data.csv')
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.9515656001891349
-exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=1, max_features=0.7000000000000001, min_samples_leaf=12, min_samples_split=2, n_estimators=100, subsample=0.9000000000000001)
+# Average CV score on the training set was: 0.9513211014116288
+exported_pipeline = KNeighborsClassifier(n_neighbors=32, p=2, weights="distance")
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
